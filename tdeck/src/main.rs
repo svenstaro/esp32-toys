c68ec6f46543095e33e2ec0f47696a8bed49015e
Sven-Hendrik Haase <svenstaro@gmail.com>
2024-03-09 07:27:08 +0100

    Using framebuffer but not perfect yet

--- tdeck/src/main.rs
+++ tdeck/src/main.rs
@@ -1,18 +1,21 @@
 use std::time::Duration;
 
 use anyhow::Result;
+use display_interface_spi::SPIInterfaceNoCS;
+use embedded_graphics::pixelcolor::{Rgb565, Rgb888};
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{Circle, Line, PrimitiveStyle, Rectangle};
+use embedded_graphics_framebuf::FrameBuf;
 use esp_idf_hal::delay::Ets;
 use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::spi::{SpiDeviceDriver, SpiDriverConfig, SpiError};
 use esp_idf_sys::EspError;
+
 // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use display_interface_spi::SPIInterfaceNoCS;
-use embedded_graphics::pixelcolor::{Rgb565, Rgb888};
-use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{Circle, Line, PrimitiveStyle};
 use esp_idf_sys as _;
+
 use log::*;
 use mipidsi::{Builder, Orientation};
 use thiserror::Error;
@@ -59,23 +62,23 @@ fn main() -> Result<()> {
         sdo,
         Some(sdi),
         Some(cs),
-        &SpiDriverConfig::new(),
+        &SpiDriverConfig::new().dma(esp_idf_hal::spi::Dma::Auto(4096)),
         &config,
     )?;
 
     // display interface abstraction from SPI and DC
     let di = SPIInterfaceNoCS::new(device, dc);
 
-    // create driver
+    // turn on the backlight
+    backlight.set_high()?;
+
+    // create display driver
     let mut display = Builder::st7789(di)
         .with_invert_colors(mipidsi::ColorInversion::Inverted)
         .with_display_size(320, 240)
         .with_orientation(Orientation::Landscape(true))
         .init(&mut delay, Some(rst))
         .unwrap();
-
-    // turn on the backlight
-    backlight.set_high()?;
 
     // let raw_image_data = ImageRawLE::new(include_bytes!("../examples/assets/ferris.raw"), 86);
     // let ferris = Image::new(&raw_image_data, Point::new(0, 0));
@@ -100,27 +103,38 @@ fn main() -> Result<()> {
     //     // display.clear(Rgb565::BLACK).unwrap();
     //     // std::thread::sleep(Duration::from_millis(500));
     // }
+
     let omg = std::thread::Builder::new()
-        .stack_size(4096 * 2)
-        .spawn(move || loop {
-            for y in 0..240 {
-                let rainbow_pos = (y as f32 / 240.0) as f64;
-                let color_at = g.at(rainbow_pos).to_rgba8();
-                let rgb = Rgb888::new(color_at[0], color_at[1], color_at[2]);
-                Line::new(Point::new(0, y), Point::new(320, y))
-                    .into_styled(PrimitiveStyle::with_stroke(rgb.into(), 1))
-                    .draw(&mut display)
+        .stack_size(4096 * 40)
+        .spawn(move || {
+            let mut framebuffer_data: [Rgb565; 320 * 240] = [Rgb565::BLACK; 320 * 240];
+            let mut fb = FrameBuf::new(&mut framebuffer_data, 320, 240);
+            loop {
+                for y in 0..240 {
+                    let rainbow_pos = (y as f32 / 240.0) as f64;
+                    let color_at = g.at(rainbow_pos).to_rgba8();
+                    let rgb = Rgb888::new(color_at[0], color_at[1], color_at[2]);
+                    Line::new(Point::new(0, y), Point::new(320, y))
+                        .into_styled(PrimitiveStyle::with_stroke(rgb.into(), 1))
+                        .draw(&mut display)
+                        .unwrap();
+                }
+                Circle::with_center(Point::new(0, 0), 30)
+                    .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+                    .draw(&mut fb)
                     .unwrap();
+                Circle::with_center(Point::new(320, 240), 30)
+                    .into_styled(PrimitiveStyle::with_fill(Rgb565::BLUE))
+                    .draw(&mut fb)
+                    .unwrap();
+                let area = Rectangle::new(Point::new(0, 0), fb.size());
+                // fb.as_image().draw(&mut display).unwrap();
+
+                display
+                    .fill_contiguous(&area, fb.data.iter().copied())
+                    .unwrap();
+                std::thread::sleep(Duration::from_millis(100));
             }
-            Circle::with_center(Point::new(0, 0), 30)
-                .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
-                .draw(&mut display)
-                .unwrap();
-            Circle::with_center(Point::new(320, 240), 30)
-                .into_styled(PrimitiveStyle::with_fill(Rgb565::BLUE))
-                .draw(&mut display)
-                .unwrap();
-            std::thread::sleep(Duration::from_millis(500));
         })?;
     omg.join().unwrap();
 
