7d3e2ed6cbf8646133808d19f4160e9eff64d840
Sven-Hendrik Haase <svenstaro@gmail.com>
2024-03-09 05:34:04 +0100

    At last, a rainbow

--- tdeck/src/main.rs
+++ tdeck/src/main.rs
@@ -9,9 +9,9 @@ use esp_idf_hal::spi::{SpiDeviceDriver, SpiDriverConfig, SpiError};
 use esp_idf_sys::EspError;
 // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use display_interface_spi::SPIInterfaceNoCS;
-use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::pixelcolor::{Rgb565, Rgb888};
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{Circle, PrimitiveStyle};
+use embedded_graphics::primitives::{Circle, Line, PrimitiveStyle};
 use esp_idf_sys as _;
 use log::*;
 use mipidsi::{Builder, Orientation};
@@ -70,9 +70,7 @@ fn main() -> Result<()> {
     let mut display = Builder::st7789(di)
         .with_invert_colors(mipidsi::ColorInversion::Inverted)
         .with_display_size(320, 240)
-        // set default orientation
-        .with_orientation(Orientation::Portrait(false))
-        // initialize
+        .with_orientation(Orientation::Landscape(true))
         .init(&mut delay, Some(rst))
         .unwrap();
 
@@ -85,35 +83,46 @@ fn main() -> Result<()> {
     // draw image on black background
     // ferris.draw(&mut display).unwrap();
 
-    // let g = colorgrad::rainbow();
+    let g = colorgrad::sinebow();
 
-    loop {
-        // Line::new(Point::new(0, 0), Point::new(320, 240))
-        //     .into_styled(PrimitiveStyle::with_stroke(Rgb565::RED, 1))
-        //     .draw(&mut display)
-        //     .unwrap();
-        display.clear(Rgb565::RED).unwrap();
-
-        Circle::new(Point::new(10, 20), 30)
-            .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
-            .draw(&mut display)
-            .unwrap();
-        std::thread::sleep(Duration::from_millis(500));
-        // display.clear(Rgb565::BLACK).unwrap();
-        // std::thread::sleep(Duration::from_millis(500));
-    }
-    // std::thread::spawn(move || loop {
-    // for y in 0..240 {
-    //     let color_at = g.at((y / 240) as f64);
-    //     let rgb = Rgb565::new(color_at.r as u8, color_at.g as u8, color_at.b as u8);
-    //     Line::new(Point::new(0, y), Point::new(320, y))
-    //         .into_styled(PrimitiveStyle::with_stroke(rgb, 1))
+    // loop {
+    //     // Line::new(Point::new(0, 0), Point::new(320, 240))
+    //     //     .into_styled(PrimitiveStyle::with_stroke(Rgb565::RED, 1))
+    //     //     .draw(&mut display)
+    //     //     .unwrap();
+    //     display.clear(Rgb565::RED).unwrap();
+    //
+    //     Circle::new(Point::new(10, 20), 30)
+    //         .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
     //         .draw(&mut display)
     //         .unwrap();
+    //     std::thread::sleep(Duration::from_millis(500));
+    //     // display.clear(Rgb565::BLACK).unwrap();
+    //     // std::thread::sleep(Duration::from_millis(500));
     // }
-    // })
-    // .join()
-    // .unwrap();
+    let omg = std::thread::Builder::new()
+        .stack_size(4096 * 2)
+        .spawn(move || loop {
+            for y in 0..240 {
+                let rainbow_pos = (y as f32 / 240.0) as f64;
+                let color_at = g.at(rainbow_pos).to_rgba8();
+                let rgb = Rgb888::new(color_at[0], color_at[1], color_at[2]);
+                Line::new(Point::new(0, y), Point::new(320, y))
+                    .into_styled(PrimitiveStyle::with_stroke(rgb.into(), 1))
+                    .draw(&mut display)
+                    .unwrap();
+            }
+            Circle::with_center(Point::new(0, 0), 30)
+                .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
+                .draw(&mut display)
+                .unwrap();
+            Circle::with_center(Point::new(320, 240), 30)
+                .into_styled(PrimitiveStyle::with_fill(Rgb565::BLUE))
+                .draw(&mut display)
+                .unwrap();
+            std::thread::sleep(Duration::from_millis(500));
+        })?;
+    omg.join().unwrap();
 
-    // Ok(())
+    Ok(())
 }
